f90d792ce4765b8f1d79eb9aa91d2fa3723745bd
hemantic <artem.kiselev@gmail.com>
2020-08-21 10:26:06 +0300

    Celery again

--- src/app/celery.py
+++ src/app/celery.py
@@ -15,12 +15,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 celery = Celery('app')
 
 # почему-то подключение из настроек джанги не заработало, нужно будет разобраться
-celery.conf.update(
-    broker_url=env('REDIS_URL'),
-    task_always_eager=env('CELERY_ALWAYS_EAGER', cast=bool, default=False),
-    redis_max_connections=env('CELERY_REDIS_MAX_CONNECTIONS', default=None),
-    broker_transport_options={'visibility_timeout': 3600 * 48},
-    timezone=env('TIME_ZONE', cast=str, default='Europe/Moscow'),
-)
+celery.config_from_object(settings)
 
 celery.autodiscover_tasks(lambda: settings.INSTALLED_APPS)  # noqa E800
