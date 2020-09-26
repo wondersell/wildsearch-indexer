7d6744cf9eabb6e67340e517d7b767951b9dba5a
hemantic <artem.kiselev@gmail.com>
2020-09-26 04:04:13 +0300

    Altered processing logic in prepare_dump and export_dump backrgound tasks: now failed task will not set next task

--- src/wdf/tasks.py
+++ src/wdf/tasks.py
@@ -19,16 +19,16 @@ logger.setLevel(logging.INFO)
 def prepare_dump(job_id):
     logger.info(f'Preparing dump for job {job_id}')
 
-    indexer = Indexer(get_chunk_size=5000, save_chunk_size=5000)
+    indexer = Indexer(get_chunk_size=5000)
 
     try:
         indexer.prepare_dump(job_id=job_id)
     except DumpStateTooLateError as e:
         logger.error(f'Job {job_id} import failed. {str(e)}')
+    else:
+        logger.info(f'Dump for job {job_id} prepared, adding import task')
 
-    logger.info(f'Dump for job {job_id} prepared, adding import task')
-
-    import_dump.delay(job_id=job_id)
+        import_dump.delay(job_id=job_id)
 
 
 @shared_task(
@@ -41,11 +41,11 @@ def prepare_dump(job_id):
 def import_dump(job_id):
     logger.info(f'Importing dump for job {job_id}')
 
-    indexer = Indexer(get_chunk_size=1000, save_chunk_size=1000)
+    indexer = Indexer(get_chunk_size=5000)
 
     try:
         indexer.import_dump(job_id=job_id)
     except DumpStateTooLateError as e:
         logger.error(f'Job {job_id} import failed. {str(e)}')
-
-    logger.info(f'Dump for job {job_id} imported')
+    else:
+        logger.info(f'Dump for job {job_id} imported')
