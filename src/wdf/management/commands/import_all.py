b0e45de5cd8a6079809a0edb6b861978ccca8314
hemantic <artem.kiselev@gmail.com>
2020-10-23 13:30:35 +0300

    Import all task updated

--- src/wdf/management/commands/import_all.py
+++ src/wdf/management/commands/import_all.py
@@ -1,9 +1,12 @@
 import logging
+from celery import chain, chord
 from django.conf import settings
 from django.core.management.base import BaseCommand
+from math import ceil
 from scrapinghub import ScrapinghubClient
 
-from wdf.tasks import import_dump
+from wdf.indexer import Indexer
+from wdf.tasks import import_dump, prepare_dump, wrap_dump
 
 
 class Command(BaseCommand):
@@ -25,5 +28,24 @@ class Command(BaseCommand):
 
         for job in client.get_project(settings.SH_PROJECT_ID).jobs.iter(has_tag=options['tags'].split(','), state=options['state']):
             job_id = job['key']
-            import_dump.delay(job_id=job_id)
-            self.stdout.write(self.style.SUCCESS(f'Job #{job_id} added to process queue'))
+
+            indexer = Indexer(job_id=job_id)
+
+            group_size = options['group_size']
+
+            if options['chunk_size']:
+                indexer.set_chunk_size_get(options['chunk_size'])
+                indexer.set_chunk_size_save(options['chunk_size'])
+
+            tasks_num = ceil(indexer.dump.items_crawled / group_size)
+
+            chain(
+                prepare_dump.s(job_id=job_id),
+                chord(
+                    [import_dump.s(start=group_size * i, count=group_size) for i in range(tasks_num)],
+                    wrap_dump.s(job_id=job_id),
+                ),
+            ).apply_async(expires=24 * 60 * 60)
+
+            self.stdout.write(self.style.SUCCESS(
+                f'Job #{job_id} added to process queue for import ({tasks_num} tasks with up to {group_size} items each)'))
