5d2f59b1cdb609712d992a58757cb19bad98d105
hemantic <artem.kiselev@gmail.com>
2020-12-04 16:24:29 +0300

    ECS instance sizes optimized

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -52,7 +52,7 @@ locals {
 
   ecs_ami             = "ami-03fc956d7468aa8a1"
   ecs_instance_size   = "t3.micro"
-  ecs_instances_count = 3
+  ecs_instances_count = 1
 
   availability_zones  = ["eu-north-1a", "eu-north-1b", "eu-north-1c"]
   vpc_cidr            = "10.0.0.0/16"
@@ -77,7 +77,7 @@ resource "aws_ecs_cluster" "cluster" {
 
 resource "aws_ecs_service" "celery" {
   name          = local.aws_ecs_service_celery_name
-  desired_count = 5
+  desired_count = 1
 
   cluster         = aws_ecs_cluster.cluster.id
   task_definition = aws_ecs_task_definition.celery.arn
